9227dab5b195ebffd052edab09694f47b324cb46
Abdulqadeerhariri <kinghariri700800@gmail.com>
2026-02-17 08:04:05 +0430

    react router

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Home />
+    
+    <Footer />
+    </>
+  )
+}
+
+export default App
